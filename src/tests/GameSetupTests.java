c9c4d6978b406043f2b28cd40f09a7523589e76b
Dylan Eberhard <Dylan Eberhard>
2016-10-26 17:58:04 -0700

    forgot to add in test files

---
+++ src/tests/GameSetupTests.java
@@ -0,0 +1,131 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+import java.util.Set;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+import clueGame.Card;
+import clueGame.CardType;
+import clueGame.ComputerPlayer;
+import clueGame.HumanPlayer;
+
+public class GameSetupTests {
+	// variable for board
+	private static Board board;
+	
+	// @Before to initialize board
+	@BeforeClass
+	public static void setUp() {
+		// Board is singleton, get the only instance and initialize it		
+		board = Board.getInstance();
+		board.setConfigFiles("TCJPClueLayout.csv", "TCJPClueLayoutLegend.txt", "ClueCards.txt", "Players.txt");
+		board.initialize();
+	}
+	
+	// test for loading the players
+	@Test
+	public void loadingThePeople() {
+		// variable for the test player
+		HumanPlayer testHumanPlayer = board.gethumanPlayer();
+		// variable for the computer players
+		ComputerPlayer[] testComputerArray = board.getComputerPlayers();
+		
+		// Ensure players name is John Doe
+		assertEquals("John Doe", testHumanPlayer.getPlayerName());
+		// Ensure fist computer player is Jane Doe
+		assertEquals("Jane Doe", testComputerArray[0].getPlayerName());
+		// Ensure last computer player is Janet Fawn
+		assertEquals("Janet Fawn", testComputerArray[4].getPlayerName());
+		// Ensure human's color is orange
+		assertEquals(Color.orange, testHumanPlayer.getColor());
+		// Ensure first computer player is magenta
+		assertEquals(Color.magenta, testComputerArray[0].getColor());
+		// Ensure fifth computer player is yellow
+		assertEquals(Color.yellow, testComputerArray[4].getColor());
+		// Determine player is in row 18
+		assertEquals(18, testHumanPlayer.getRow());
+		// Determine the first computer player is in row 24
+		assertEquals(24, testComputerArray[0].getRow());
+		// Determine the fifth computer player is in row 0
+		assertEquals(0, testComputerArray[4].getRow());
+		// Determine player is in column 0
+		assertEquals(0, testHumanPlayer.getColumn());
+		// Determine the first computer player is in column 10
+		assertEquals(10, testComputerArray[0].getColumn());
+		// Determine the fifth computer player is in column 3
+		assertEquals(3, testComputerArray[4].getColumn());
+	}
+	
+	// test for loading the deck of cards
+	@Test
+	public void loadingTheDeckOfCards(){
+		Set<Card> deckOfCards = board.getDeckOfCards();
+
+		// size of deck is 21 cards
+		assertEquals(21, deckOfCards.size());
+		// Ensure there are the correct number of card types
+		assertEquals(9, board.getNumberOfRoomCards());
+		assertEquals(6, board.getNumberOfWeaponCards());
+		assertEquals(6, board.getNumberOfPersonCards());
+		// 
+		boolean a = false;
+		boolean b = false;
+		boolean c = false;
+		// loop through deck of cards, check to see if John Doe, Cross Bow,
+		// and Gallery are all in the deck
+		for(Card currentCard: deckOfCards){
+			if(currentCard.getCardName().equals("John Doe")){
+				a = true;
+			}else if(currentCard.getCardName().equals("Cross Bow")){
+				b = true;
+			}else if(currentCard.getCardName().equals("Gallery")){
+				c = true;
+			}else{
+				
+			}
+		}
+		assert (a);
+		assert (b);
+		assert (c);
+	}
+	
+	// test for dealing the cards to players
+	@Test
+	public void dealingOfTheCards(){
+		// variable to store deck of cards
+		Card[] dealtCards = board.getDealtCards();
+		// variable to store information for computer players
+		ComputerPlayer[] temp = board.getComputerPlayers();
+		
+		// Ensure player has 3 or 4 cards
+		assert(board.gethumanPlayer().getPlayerCards().size() == 4 || board.gethumanPlayer().getPlayerCards().size() == 3);
+		for (int i = 0;i <5; i++){
+			// ensure each computer player has 3-4 cards per hand
+			assert(temp[i].getPlayerCards().size() == 4 || temp[i].getPlayerCards().size() == 3);
+		}
+
+		// ensure 21 cards were dealt
+		assertEquals(21, dealtCards.length);
+		// loop through each card
+		for (int i = 0; i < 21; i++){
+			// assign card to temporary variable
+			Card tempCard = dealtCards[i];
+			// loop through dealt cards for
+			for (int j = 0; j < 21; j++){
+				// ensure the card appears only once
+				if(j == i){	
+				}else{
+					assert(tempCard != dealtCards[j]);
+				}
+			}
+		}
+	}
+	
+	
+	
+}
---
+++ src/tests/gameActionTests.java
@@ -0,0 +1,158 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+import clueGame.BoardCell;
+import clueGame.ComputerPlayer;
+import clueGame.Solution;
+
+public class gameActionTests {
+	// variable for board
+		private static Board board;
+		
+	// @Before to initialize board
+	@BeforeClass
+	public static void setUp() {
+		// Board is singleton, get the only instance and initialize it		
+		board = Board.getInstance();
+		board.setConfigFiles("TCJPClueLayout.csv", "TCJPClueLayoutLegend.txt", "ClueCards.txt", "Players.txt");
+		board.initialize();
+	}
+	
+	@Test
+	public void testTargetWithRoom() {
+		ComputerPlayer player = new ComputerPlayer("John Doe", Color.blue , 4 , 19, board.getDeckOfCards());
+		board.calcTargets(4, 19, 3);
+		BoardCell tempDoor = board.getCellAt(7, 19);
+		System.out.println(tempDoor);
+		//System.out.println(player.pickLocation(board.getTargets()));
+		for (int i=0; i<100; i++) {		
+			player.setLastCellVisited(board.getCellAt(2, 12));
+			assertEquals(tempDoor, player.pickLocation(board.getTargets()));
+		}
+	}
+	
+	@Test
+	public void testTargetRandomSelection() {
+	    ComputerPlayer player = new ComputerPlayer("John Doe", Color.blue , 16 , 5, board.getDeckOfCards());
+	    // Pick a location with no rooms in target, just three targets
+	    board.calcTargets(16, 5, 3);
+	    boolean loc_13_5 = false;
+	    boolean loc_19_5 = false;
+	    boolean loc_18_4 = false;
+	    // Run the test a large number of times
+	    for (int i=0; i<100; i++) {
+	        BoardCell selected = player.pickLocation(board.getTargets());
+	        if (selected == board.getCellAt(13, 5))
+	            loc_13_5 = true;
+	        else if (selected == board.getCellAt(19, 5))
+	            loc_19_5 = true;
+	        else if (selected == board.getCellAt(18, 4))
+	            loc_18_4 = true;
+	        else
+	            fail("Invalid target selected");
+	        }
+	        // Ensure each target was selected at least once
+	        assertTrue(loc_13_5);
+	        assertTrue(loc_19_5);
+	        assertTrue(loc_18_4);				
+	}
+	
+	@Test
+	public void testTargetSelectionAfterLeavingRoom() {
+	    ComputerPlayer player = new ComputerPlayer("John Doe", Color.blue , 8 , 0, board.getDeckOfCards());
+	    // Pick a location with no rooms in target, just three targets
+	    player.setLastCellVisited(new BoardCell(10,0,'K'));
+	    board.calcTargets(8, 0, 2);
+	    boolean loc_6_0 = false;
+	    boolean loc_7_1 = false;
+	    boolean loc_10_0 = false;
+	    // Run the test a large number of times
+	    for (int i=0; i<100; i++) {
+	        BoardCell selected = player.pickLocation(board.getTargets());
+	        if (selected == board.getCellAt(6, 0))
+	            loc_6_0 = true;
+	        else if (selected == board.getCellAt(7, 1))
+	            loc_7_1 = true;
+	        else if (selected == board.getCellAt(10, 0))
+	            loc_10_0 = true;
+	        else
+	            fail("Invalid target selected");
+	        }
+	        // Ensure each target was selected at least once
+	        assertTrue(loc_6_0);
+	        assertTrue(loc_7_1);
+	        assertTrue(loc_10_0);				
+	}
+
+
+	@Test
+	public void checkingAccusations() {
+		board.setTheAnswer("Jim Buck", "Kitchen" , "Shot Gun");
+		Solution correctSolution = new Solution("Jim Buck", "Kitchen" , "Shot Gun");
+		assert(board.checkAccusation(correctSolution));
+		Solution wrongPerson = new Solution("John Doe", "Kitchen" , "Shot Gun");
+		assert(!(board.checkAccusation(wrongPerson)));
+		Solution wrongRoom = new Solution("Jim Buck", "Patio" , "Shot Gun");
+		assert(!(board.checkAccusation(wrongRoom)));
+		Solution wrongWeapon = new Solution("Jim Buck", "Kitchen" , "Skinning Knife");
+		assert(!(board.checkAccusation(wrongWeapon)));
+	}
+	
+	@Test
+	public void dissprovingSuggesstion() {
+		fail();
+		
+	}
+	
+	@Test
+	public void handlingSuggestion() {
+		fail();
+	}
+	
+	@Test
+	public void creatingSuggestion() {
+		ComputerPlayer[] computerPlayers = board.getComputerPlayers();
+		
+		computerPlayers[0].setColumn(3);
+		computerPlayers[0].setColumn(2);
+		computerPlayers[0].createSuggestion(board.getCellAt(computerPlayers[0].getRow(),computerPlayers[0].getColumn()), board);
+		assertEquals("Gym", computerPlayers[0].getSuggestion().room);
+		
+		//one weapon or one player has not been seen
+		computerPlayers[0].removeFromUnseenForOneWeaponandPerson();
+		computerPlayers[0].createSuggestion(board.getCellAt(computerPlayers[0].getRow(),computerPlayers[0].getColumn()), board);
+		assertEquals("Shot Gun", computerPlayers[0].getSuggestion().weapon);
+		assertEquals("John Doe", computerPlayers[0].getSuggestion().person);
+		
+		boolean shotGun = false;
+		boolean blackPowderRifle = false;
+		boolean johnDoe = false;
+		boolean joeBuck = false;
+		for(int i = 0; i < 25; i++){
+			computerPlayers[0].createSuggestion(board.getCellAt(computerPlayers[0].getRow(),computerPlayers[0].getColumn()), board);
+			if (computerPlayers[0].getSuggestion().person == "John Doe"){
+				johnDoe = true;
+			}
+			if (computerPlayers[0].getSuggestion().person == "Joe Buck"){
+				joeBuck = true;
+			}
+			if (computerPlayers[0].getSuggestion().weapon == "Black Powder Rifle"){
+				blackPowderRifle = true;
+			}
+			if (computerPlayers[0].getSuggestion().weapon == "Shot Gun"){
+				shotGun = true;
+			}
+		}
+		assert(shotGun);
+		assert(blackPowderRifle);
+		assert(joeBuck);
+		assert(johnDoe);
+	}
+}
